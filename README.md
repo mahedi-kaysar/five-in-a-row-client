365c7e01cd3174482130917fafdd09e8b44fdcf4
Md Mahedi Kaysar <mkaysar@jaguarlandrover.com>
2020-04-17 22:08:34 -0400

    added game client

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Game Client
+This is a sample client of five-in-row game server (https://github.com/mahedi-kaysar/five-in-a-row-server).
+
+## Required Software 
+* java 8+
+* Maven 3.6+
+
+## how to run
+```$xslt
+mvn clean package
+java -jar target/five-in-a-row-client-0.0.1-SNAPSHOT.jar
+```
+## Note
+
+* Please make sure server is up before running the client.
+* Run server in localhost:8080
+* If you put any invalid input please restart the server and start the client again. 
+* Because, the client did not handle the error response properly.
+* However, server handles the invalid input and send appropriate error response.
+* Please follow the readme of https://github.com/mahedi-kaysar/five-in-a-row-server for details of the endpoints.
--- src/main/java/com/example/fiveinarowclient/FiveInARowClientApplication.java
+++ src/main/java/com/example/fiveinarowclient/FiveInARowClientApplication.java
@@ -1,13 +1,15 @@
 package com.example.fiveinarowclient;
 
 import com.example.fiveinarowclient.rest.Client;
+import com.google.gson.*;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+
+import java.util.Scanner;
 
 @Log4j2
 @SpringBootApplication
@@ -15,6 +17,7 @@ public class FiveInARowClientApplication implements CommandLineRunner {
 
 	@Autowired
 	Client client;
+	Scanner sc = new Scanner(System.in);
 
 	public static void main(String[] args) {
 		new SpringApplicationBuilder(FiveInARowClientApplication.class)
@@ -25,18 +28,89 @@ public class FiveInARowClientApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 
-		try {
-			String connectionInfo1 = client.registerPlayer("Mahedi");
-			log.info(connectionInfo1);
-			String connectionInfo2 = client.registerPlayer("Kaysar");
-			log.info(connectionInfo2);
-			//client.disconnectPlayer(1);
-			String nextMove = client.nextMove(1, 5);
-			log.info(nextMove);
-			String status = client.getGameStatus();
-			log.info(status);
+		try{
+			System.out.print("[Input] Player 1: Name=");
+			registerPlayerToGame(sc.next());
+			System.out.print("[Input] Player 2: Name=");
+			registerPlayerToGame(sc.next());
 		} catch (Exception e) {
-			log.error("error in rest client", e);
+			log.info("Please make sure server is running, error=", e);
 		}
+
+		try {
+			while (isGameRunning()) {
+				printBoard();
+				playNextTurn();
+			}
+			printGameResult();
+		} catch (Exception e) {
+			log.info("Please restart your server, if your input was wrong, error=", e);
+		}
+
+	}
+
+	private void printGameResult() {
+		System.out.println("Result Summary:");
+		JsonObject response = new JsonParser()
+				.parse(client.getGameStatus()).getAsJsonObject();
+
+		String gameStatus = response.get("payload")
+				.getAsJsonObject().get("gameStatus").getAsString();
+		System.out.println(String.format("GameStatus:%s",gameStatus));
+		if(gameStatus.equals("FINISHED")) {
+			String winner = response.get("payload").getAsJsonObject().get("champion").getAsJsonObject()
+					.get("name").getAsString();
+			String color = response.get("payload").getAsJsonObject().get("champion").getAsJsonObject()
+					.get("color").getAsString();
+			System.out.println(String.format("Winner: %s (%s)",winner, color));
+		}
+	}
+
+	private void registerPlayerToGame(String name) {
+		client.registerPlayer(name);
+	}
+
+	private void playNextTurn() {
+		JsonObject response = new JsonParser()
+				.parse(client.getGameStatus()).getAsJsonObject();
+
+		String playerNameToNextTurn = response.get("payload").getAsJsonObject()
+				.get("playerToNextTurn").getAsJsonObject()
+				.get("name").getAsString();
+		int totalColumn = response.get("payload").getAsJsonObject()
+				.get("board").getAsJsonObject().get("totalColumn").getAsInt();
+
+		System.out.println(String.format("It's your turn %s, please enter column (1-%s): ", playerNameToNextTurn, totalColumn));
+		int inputColumn = sc.nextInt();
+
+		int playerIdToNextTurn = response.get("payload").getAsJsonObject()
+				.get("playerToNextTurn").getAsJsonObject()
+				.get("id").getAsInt();
+		this.client.nextMove(playerIdToNextTurn, inputColumn);
+	}
+
+	private void printBoard() {
+		JsonObject response = new JsonParser()
+				.parse(client.getGameStatus()).getAsJsonObject();
+		JsonArray jsonBoardTable = response.get("payload").getAsJsonObject()
+				.get("board").getAsJsonObject().get("table").getAsJsonArray();
+		System.out.println("=============Game Board============");
+		for(JsonElement row: jsonBoardTable) {
+			for(int i = 1; i < row.getAsJsonArray().size(); i++) {
+				System.out.print(String.format("[%s]", row.getAsJsonArray().get(i)));
+			}
+			System.out.println();
+		}
+	}
+
+	private boolean isGameRunning() {
+		JsonObject response = new JsonParser()
+				.parse(client.getGameStatus()).getAsJsonObject();
+		String gameStatus = response.get("payload")
+				.getAsJsonObject().get("gameStatus").getAsString();
+		if(gameStatus.equals("INITIALISED")) {
+			return true;
+		}
+		return false;
 	}
 }
